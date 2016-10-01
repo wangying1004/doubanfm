49e2a4a7632d2346609b0aa8e1ed88c5b4de5ad1
wangying <wwwww@mail.com>
2016-10-01 14:26:14 +0800

    导航栏

---
+++ 1.js
@@ -0,0 +1,11 @@
+(function() {
+    var xiala = document.getElementById("xiala");
+    var fufei = document.getElementById('fufei');
+    xiala.onmouseover = function() {
+        fufei.style.display='block';
+    }
+    xiala.onmouseout= function() {
+        fufei.style.display='none';
+    }
+
+}(window))
