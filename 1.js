510a45bce97c657826d9226a9094ac0523951fb1
wangying <wwwww@mail.com>
2016-10-02 03:38:08 +0800

    add 滚动 and点击事件 and悬停换图

--- 1.js
+++ 1.js
@@ -37,6 +37,14 @@
     shanchu.onmouseout = function() {
         document.getElementById('scg').style.display = 'none';
     }
+    var play = document.getElementById('play');
+    play.onclick=function(){
+    	document.getElementById('playerfu').style.display="block";
+    }
+    var contplay=document.getElementById('contplay');
+    contplay.onclick=function(){
+    	document.getElementById('playerfu').style.display='none';
+    }
     /*var btn = document.getElementById('btn');
     btn.onclick = function() {
         var box = document.getElementById('box');
@@ -45,4 +53,129 @@
         box.style.left += x;
     }
 */
+    var xta =document.getElementById('xta');
+    var xtaa =document.getElementById('xtaa');
+    var xtb =document.getElementById('xtb');
+    var xtbb =document.getElementById('xtbb');
+    var xtc =document.getElementById('xtc');
+    var xtcc =document.getElementById('xtcc');
+    var xtd =document.getElementById('xtd');
+    var xtdd =document.getElementById('xtdd');
+    var xte =document.getElementById('xte');
+    var xtee =document.getElementById('xtee');
+    var xtf =document.getElementById('xtf');
+    var xtff =document.getElementById('xtff');
+    xta.onmouseover=function(){
+    	xtaa.style.display="block";
+    	document.getElementById('a').style.display="block";
+    	document.getElementById('b').style.display="none";
+    	document.getElementById('c').style.display="none";
+    	document.getElementById('d').style.display="none";
+    	document.getElementById('e').style.display="none";
+    	document.getElementById('f').style.display="none";
+    	document.getElementById('wa').style.display="block";
+    	document.getElementById('wb').style.display="none";
+    	document.getElementById('wc').style.display="none";
+    	document.getElementById('wd').style.display="none";
+    	document.getElementById('we').style.display="none";
+    	document.getElementById('wf').style.display="none";
+    }
+     xta.onmouseout=function(){
+    	xtaa.style.display="none";
+    }
+    xtb.onmouseover=function(){
+    	xtbb.style.display="block";
+    	document.getElementById('b').style.display="block";
+    	document.getElementById('a').style.display="none";
+    	document.getElementById('c').style.display="none";
+    	document.getElementById('d').style.display="none";
+    	document.getElementById('e').style.display="none";
+    	document.getElementById('f').style.display="none";
+    	document.getElementById('wb').style.display="block";
+    	document.getElementById('wa').style.display="none";
+    	document.getElementById('wc').style.display="none";
+    	document.getElementById('wd').style.display="none";
+    	document.getElementById('we').style.display="none";
+    	document.getElementById('wf').style.display="none";
+
+    }
+    xtb.onmouseout=function(){
+    	xtbb.style.display="none";
+    }
+    xtc.onmouseover=function(){
+    	xtcc.style.display="block";
+    	document.getElementById('c').style.display="block";
+    	document.getElementById('a').style.display="none";
+    	document.getElementById('b').style.display="none";
+    	document.getElementById('d').style.display="none";
+    	document.getElementById('e').style.display="none";
+    	document.getElementById('f').style.display="none";
+    	document.getElementById('wc').style.display="block";
+    	document.getElementById('wb').style.display="none";
+    	document.getElementById('wa').style.display="none";
+    	document.getElementById('wd').style.display="none";
+    	document.getElementById('we').style.display="none";
+    	document.getElementById('wf').style.display="none";
+
+    }
+    xtc.onmouseout=function(){
+    	xtcc.style.display="none";
+    }
+    xtd.onmouseover=function(){
+    	xtdd.style.display="block";
+    	document.getElementById('d').style.display="block";
+    	document.getElementById('a').style.display="none";
+    	document.getElementById('b').style.display="none";
+    	document.getElementById('c').style.display="none";
+    	document.getElementById('e').style.display="none";
+    	document.getElementById('f').style.display="none";
+    	document.getElementById('wd').style.display="block";
+    	document.getElementById('wb').style.display="none";
+    	document.getElementById('wc').style.display="none";
+    	document.getElementById('wa').style.display="none";
+    	document.getElementById('we').style.display="none";
+    	document.getElementById('wf').style.display="none";
+
+    }
+    xtd.onmouseout=function(){
+    	xtdd.style.display="none";
+    }
+     xte.onmouseover=function(){
+    	xtee.style.display="block";
+    	document.getElementById('e').style.display="block";
+    	document.getElementById('a').style.display="none";
+    	document.getElementById('b').style.display="none";
+    	document.getElementById('c').style.display="none";
+    	document.getElementById('d').style.display="none";
+    	document.getElementById('f').style.display="none";
+    	document.getElementById('we').style.display="block";
+    	document.getElementById('wb').style.display="none";
+    	document.getElementById('wc').style.display="none";
+    	document.getElementById('wd').style.display="none";
+    	document.getElementById('wa').style.display="none";
+    	document.getElementById('wf').style.display="none";
+
+    }
+    xte.onmouseout=function(){
+    	xtee.style.display="none";
+    }
+    xtf.onmouseover=function(){
+    	xtff.style.display="block";
+    	document.getElementById('f').style.display="block";
+    	document.getElementById('a').style.display="none";
+    	document.getElementById('b').style.display="none";
+    	document.getElementById('c').style.display="none";
+    	document.getElementById('d').style.display="none";
+    	document.getElementById('e').style.display="none";
+    	document.getElementById('wf').style.display="block";
+    	document.getElementById('wb').style.display="none";
+    	document.getElementById('wc').style.display="none";
+    	document.getElementById('wd').style.display="none";
+    	document.getElementById('we').style.display="none";
+    	document.getElementById('wa').style.display="none";
+
+    }
+    xtf.onmouseout=function(){
+    	xtff.style.display="none";
+    }
 }(window))
