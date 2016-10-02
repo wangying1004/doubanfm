f352b0bbdc4c1fdee09ae21165e1ee1f3292501e
wangying <wwwww@mail.com>
2016-10-02 22:24:41 +0800

    add audio and div整体

--- 1.js
+++ 1.js
@@ -8,6 +8,47 @@
         fufei.style.display = 'none';
     }
 
+    
+    var jindutiao =document.getElementById('jindutiao');
+    var jindu=document.getElementById('jindu');
+    var fristaudio=document.getElementById('fristaudio');
+      fristaudio.ontimeupdate=function(){
+     var baifen = fristaudio.currentTime / fristaudio.duration;
+        jindutiao.style.width = baifen *230+ "px";
+        }
+     jindu.onmousedown = function(event) {
+        var length = event.clientX - jindutiao.offsetLeft;
+        var percent = (length-792) / jindu.offsetWidth; //可视宽度
+        jindutiao.style.width = percent * 230 + "px";
+        fristaudio.currentTime = percent * fristaudio.duration; //当前时间/视频持续时间
+    }
+    jindutiao.onmousedown = function(event) {
+            var length = event.clientX - jindutiao.offsetLeft;
+            var percent = (length-792) / jindu.offsetWidth;
+            jindutiao.style.width = percent * 230 + "px";
+            fristaudio.currentTime = percent * fristaudio.duration;
+        }
+        var nextjindutiao =document.getElementById('nextjindutiao');
+    var nextjindu=document.getElementById('nextjindu');
+    var nextaudio=document.getElementById('nextaudio');
+      nextaudio.ontimeupdate=function(){
+     var baifen = nextaudio.currentTime / nextaudio.duration;
+        nextjindutiao.style.width = baifen *230+ "px";
+        }
+     nextjindu.onmousedown = function(event) {
+        var length = event.clientX - nextjindutiao.offsetLeft;
+        var percent = (length-792) / nextjindu.offsetWidth; //可视宽度
+        nextjindutiao.style.width = percent * 230 + "px";
+        nextaudio.currentTime = percent * nextaudio.duration; //当前时间/视频持续时间
+    }
+    nextjindutiao.onmousedown = function(event) {
+            var length = event.clientX - nextjindutiao.offsetLeft;
+            var percent = (length-792) / nextjindu.offsetWidth;
+            nextjindutiao.style.width = percent * 230 + "px";
+            nextaudio.currentTime = percent * nextaudio.duration;
+        }
+
+
     var shoucang = document.getElementById('shoucang');
     var like = document.getElementById('like');
     shoucang.onclick = function() {
@@ -26,13 +67,49 @@
     var nextplay = document.getElementById('nextplay');
     var gemingtu = document.getElementById('gemingtu');
     var zhuanjitu = document.getElementById('zhuanjitu');
+    var playfu= document.getElementById('playfu');
+    var nextplayfu=document.getElementById('nextplayfu');
     nextplay.onclick = function() {
-        if (gemingtu.style.display == 'block' && zhuanjitu.style.display == 'block') {
+        if (gemingtu.style.display == 'block' && zhuanjitu.style.display == 'block'&&nextaudio.play()&&nextjindu.style.display=="block"&&nextjindutiao.style.display=="block"&&playfu.style.display=='block'&&nextplayfu.style.display=='block') {
             gemingtu.style.display = 'none';
             zhuanjitu.style.display = 'none';
+            nextaudio.pause();
+            fristaudio.play();
+            nextjindu.style.display='none';
+            nextjindutiao.style.display='none';
+            playfu.style.display='none';
+            nextplayfu.style.display='none';
         } else {
             gemingtu.style.display = 'block';
             zhuanjitu.style.display = 'block';
+            nextaudio.play();
+            fristaudio.pause();
+            nextjindu.style.display='block';
+            nextjindutiao.style.display='block';
+            playfu.style.display='block';
+            nextplayfu.style.display='block';
+        }
+
+    }
+    nextplayfu.onclick = function() {
+        if (geming.style.display == 'none' && zhuanji.style.display == 'none'&&fristaudio.pause()&&jindu.style.display=="none"&&jindutiao.style.display=="none"&&play.style.display=='none'&&nextplay.style.display=='none') {
+            geming.style.display = 'block';
+            zhuanji.style.display = 'block';
+            fristaudio.play();
+            nextaudio.pause();
+            jindu.style.display='block';
+            jindutiao.style.display='block';
+            play.style.display='block';
+            nextplay.style.display='block';
+        } else {
+            geming.style.display = 'none';
+            zhuanji.style.display = 'none';
+            fristaudio.pause();
+            nextaudio.play();
+            jindu.style.display='none';
+            jindutiao.style.display='none';
+            play.style.display='none';
+            nextplay.style.display='none';
         }
 
     }
@@ -53,19 +130,36 @@
     var play = document.getElementById('play');
     play.onclick=function(){
     	document.getElementById('playerfu').style.display="block";
+        fristaudio.pause();
     }
     var contplay=document.getElementById('contplay');
     contplay.onclick=function(){
     	document.getElementById('playerfu').style.display='none';
+    	 fristaudio.play();
     }
-    /*var btn = document.getElementById('btn');
-    btn.onclick = function() {
-        var box = document.getElementById('box');
-        var x = 5 px;
-        var y = 5 px;
-        box.style.left += x;
+    var playfu = document.getElementById('playfu');
+    playfu.onclick=function(){
+    	document.getElementById('playerfus').style.display="block";
+        nextaudio.pause();
     }
-*/
+    var contplays=document.getElementById('contplays');
+    contplays.onclick=function(){
+    	document.getElementById('playerfus').style.display='none';
+    	 nextaudio.play();
+    }
+    /* var box=document.getElementById('box');
+    var btn = document.getElementById('tbtn');
+   
+    tbtn.onclick=function(){
+    	if (box.style.display=='none') {
+    		box.style.display='block';
+
+    	}else {
+    		box.style.display='none';
+    		var x=10;
+    		btn.style.width-=x;
+    	}
+    }*/
     var xta =document.getElementById('xta');
     var xtaa =document.getElementById('xtaa');
     var xtb =document.getElementById('xtb');
